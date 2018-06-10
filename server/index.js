e9ba5f774b4b3c790d4210bb3500f7c834cac8c8
haochengz <haochengzhao@outlook.com>
2018-06-10 22:42:38 +0800

    some project configurations

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 import Koa from 'koa'
 import { Nuxt, Builder } from 'nuxt'
 
-async function start () {
+async function start() {
   const app = new Koa()
   const host = process.env.HOST || '127.0.0.1'
-  const port = process.env.PORT || 3000
+  const port = process.env.PORT || 3006
 
   // Import and Set Nuxt.js options
   let config = require('../nuxt.config.js')
